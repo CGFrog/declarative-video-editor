508322c0b5d1d2d1f599e52ab5b29f8d6a244980
CameronAron <cameron.aron04@gmail.com>
2026-03-09 15:05:30 -0500

    rowspan

--- src/ui/Window.py
+++ src/ui/Window.py
@@ -4,29 +4,31 @@ class Window:
     def __init__(self):
         pass
 
-root = tk.Tk()
+    def CreateWindow(self):
+        root = tk.Tk()
 
-root.title("Declarative Video Editor")
-root.geometry("400x300")
+        root.title("Declarative Video Editor")
+        root.geometry("400x300")
 
-root.rowconfigure(0, weight=1)
-root.columnconfigure(0, weight=1)
+        root.rowconfigure(0, weight=1)
+        root.columnconfigure(0, weight=1)
 
-main_frame = tk.Frame(root, bg ="YELLOW")
-main_frame.grid(row=0, column=0, sticky="nsew")
+        main_frame = tk.Frame(root, bg ="YELLOW")
+        main_frame.grid(row=0, column=0, sticky="nsew")
 
-main_frame.rowconfigure(0, weight=1)
-main_frame.rowconfigure(1, weight=1)
-main_frame.columnconfigure(0, weight=1)
-main_frame.columnconfigure(1, weight=1)
+        main_frame.rowconfigure(0, weight=1)
+        main_frame.rowconfigure(1, weight=1)
+        main_frame.columnconfigure(0, weight=1)
+        main_frame.columnconfigure(1, weight=1)
 
-left_frame = tk.Frame(main_frame, bg ="BLUE")
-left_frame.grid(row=0, column=0, sticky="nsew")
+        left_frame = tk.Frame(main_frame, bg ="BLUE")
+        left_frame.grid(row=0, rowspan=2, column=0, sticky="nsew")
 
-right_frame = tk.Frame(main_frame, bg ="GREEN")
-right_frame.grid(row=0, column=1, sticky="nsew")
+        right_frame = tk.Frame(main_frame, bg ="GREEN")
+        right_frame.grid(row=0, column=1, sticky="nsew")
 
-bottom_frame = tk.Frame(main_frame, bg = "RED")
-bottom_frame.grid(row=1, column=0, sticky="nsew")
+        bottom_frame = (
+        tk.Frame(main_frame, bg = "RED"))
+        bottom_frame.grid(row=1, column=1, sticky="nsew")
 
-root.mainloop()
+        root.mainloop()
