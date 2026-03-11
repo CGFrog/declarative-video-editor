5b6a8f3beb9a123728d71613a114d60e1d17d00c
ben-burie <benburie22@gmail.com>
2026-03-10 21:40:15 -0500

    Pair tokens with their character counterparts and create class definitions for token and lexer classes

--- src/compiler/lexer/Lexer.py
+++ src/compiler/lexer/Lexer.py
@@ -1,28 +1,39 @@
 tokens = (
     # OPERATORS
-    'ASSIGN', # assignment                =
-    'UNION', # union                      +
-    'FUNC_COMP', # function composition   |>
-    'TIMESTAMP', # timestamp              (X:XX.XX)
+    ('ASSIGN', '='),         # assignment                
+    ('UNION', '+'),                 # union                    
+    ('FUNC_COMP', '|>'),             # function composition 
 
     # TYPES
-    'VIDEO', # video
-    'AUDIO', # audio
-    'IMAGE', # image
+    ('VIDEO', 'video'), 
+    ('AUDIO', 'audio'), 
+    ('IMAGE', 'image'), 
 
     # EFFECTS
-    'BLUR', # blur
-    'SATURATION', # saturation
-    'CHROMA', # chroma key
+    ('BLUR', 'blur'), # blur
+    ('SATURATION', 'saturation'), # saturation
+    ('CHROMA', 'chroma'), # chroma key
 
     # BRACKETS & MISC.
-    'LPAREN', # (
-    'RPAREN', # )
-    'QOUTES', # "
-    'COMMA', # ,
-    'NUMBER' # XX
+    ('LPAREN', '('),
+    ('RPAREN', ')'),
+    ('QOUTES', '"'),
+    ('COMMA', ','),
+    ('COLON', ':'),
+    ('NUMBER', '0') # need to figure this one out
 )
+
+class Token():
+
+    def __init__(self, category, value):
+        self.category = category
+        self.value = value
+
+    def toString(self):
+        return f"Token({self.category}, {self.value})"
 
 class Lexer():
 
-    pass
+    def __init__(self, text):
+        self.text = text
+        self.pos = 0
