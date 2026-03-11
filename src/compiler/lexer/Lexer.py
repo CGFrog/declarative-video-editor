0565215401cb02c4e9f34270bbad3c05857eb5d2
ben-burie <benburie22@gmail.com>
2026-03-10 21:20:56 -0500

    Define tokens

--- src/compiler/lexer/Lexer.py
+++ src/compiler/lexer/Lexer.py
@@ -1,2 +1,28 @@
+tokens = (
+    # OPERATORS
+    'ASSIGN', # assignment                =
+    'UNION', # union                      +
+    'FUNC_COMP', # function composition   |>
+    'TIMESTAMP', # timestamp              (X:XX.XX)
+
+    # TYPES
+    'VIDEO', # video
+    'AUDIO', # audio
+    'IMAGE', # image
+
+    # EFFECTS
+    'BLUR', # blur
+    'SATURATION', # saturation
+    'CHROMA', # chroma key
+
+    # BRACKETS & MISC.
+    'LPAREN', # (
+    'RPAREN', # )
+    'QOUTES', # "
+    'COMMA', # ,
+    'NUMBER' # XX
+)
+
 class Lexer():
+
     pass
