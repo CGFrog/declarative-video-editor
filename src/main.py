16e6591cc8a369b7c3c0b59c2e5e74817601f6a6
CameronAron <cameron.aron04@gmail.com>
2026-03-12 20:04:54 -0500

    Fixed merge conflict

--- src/main.py
+++ src/main.py
@@ -5,6 +5,14 @@ def main()->None:
     window = Window()
     window.run()
 
+    """
+    Creation of tkinter widgets would go here (make sure to import classes at the top)
+    e.x. textEditor.CreateTextBox(window.left_frame)
+         videoPlayer.ShowMediaViewer(window.right_frame)
+    """
+
+    window.root.mainloop()
+
 
 try:
     if __name__ == "__main__":
