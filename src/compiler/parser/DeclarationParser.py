ed7cc7f5657abd098b05c7c9a9ce71986ad879d9
Ian Cooper <iancoop25@gmail.com>
2026-04-06 16:19:09 -0500

    Fix effects not appearing

--- src/compiler/parser/DeclarationParser.py
+++ src/compiler/parser/DeclarationParser.py
@@ -58,8 +58,7 @@ class DeclarationParser():
                     if input_len < 0:
                         raise(Exception(f"Invalid function composition on {self.line_number}"))
                     effects.append(Effect(token.value, self.__extract_function_parameters(tokens[index+1:index+ input_len:])))
-                    index=input_len
-                    break
+                    index=input_len+ index
             index+=1
         return effects
     
--- src/compiler/parser/TimelineParser.py
+++ src/compiler/parser/TimelineParser.py
@@ -1,3 +1,3 @@
 class TimelineParser():
-    def __init__(self, tokens):
+    def __init__(self):
         pass
