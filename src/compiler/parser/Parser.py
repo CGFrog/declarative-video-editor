5817fda05d575a25d6342feb9ba04b23c0821297
Ian Cooper <iancoop25@gmail.com>
2026-04-05 23:19:37 -0500

    Add test case

--- src/compiler/parser/Parser.py
+++ src/compiler/parser/Parser.py
@@ -16,7 +16,6 @@ class Parser():
         declaration_tokens = []
         timeline_tokens = []
 
-
         timeline_index = 0
         for line in lines_of_code:
             tokens = self.lexer.build_tokens(line)
@@ -33,9 +32,31 @@ class Parser():
             self.line_number += 1
 
         declaration_parser = DeclarationParser()
-        timeline_parser = TimelineParser()
+        # timeline_parser = TimelineParser()
 
         self.state = declaration_parser.parse_source(declaration_tokens)
         # generate state step here
         # generate timeline step here
-        
+
+def main():
+    source_code =   "video intro = \"intro.mp4\" (0,e) |> saturation(3) |> speed(1.5)"\
+                    "audio music = \"music.mp3\" (0,e) |> volume(2) |> noise_filter(-60)"\
+                    "video game_footage = \"game_footage.mp4\" (0,30) + (35,49)"\
+                    "video webcam_footage = \"webcam_footage.mp4\" (0,30) + (35,49) |> transform(1000,320) |> scale(0.2,0.2)"\
+                    ""\
+                    ""\
+                    "timeline"\
+                    "intro 0 1"\
+                    "game_footage after intro 1"\
+                    "music 0 1"\
+                    "webcam_footage after intro 2"\
+                    ""\
+                    "render \"lets_play.mp4\" [1920,1080]"
+    lexer = Lexer()
+    parser = Parser(lexer)
+    parser.parse_source(source_code)
+    print(parser)
+
+
+if __name__ == '__main__':
+    main()
