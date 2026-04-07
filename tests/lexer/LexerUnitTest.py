a583bc911e9d4de072eb44d3a5d42841b09cebd2
ben-burie <benburie22@gmail.com>
2026-04-07 16:30:27 -0500

    Add workflows yml file and test case logic script with assertations

--- tests/lexer/LexerUnitTest.py
+++ tests/lexer/LexerUnitTest.py
@@ -1,22 +1,13 @@
 import yaml
-
 from src.compiler.lexer.Lexer import Lexer
-
-lines_to_test = [
-    'video intro = "intro.mp4" (0,e) |> saturation(3) |> speed(1.5)',
-    'video webcam_footage = "webcam_footage.mp4" (0,30) + (35,49) |> transform(1000,320) |> scale(0.2,0.2)',
-    'game_footage after intro 1',
-    'render "lets_play.mp4" [1920,1080]'
-]
 
 class LexerUnitTest():
 
     def __init__(self):
         pass
 
-    def run_tests(self, line):
+    def run_test(self, line):
         output_stream = []
-        print(f"RUNNING LINE {line}\n")
         lexer = Lexer(line)
         token_stream = lexer.build_tokens();
         for token in token_stream:
@@ -27,7 +18,16 @@ class LexerUnitTest():
 if __name__ == "__main__":
     ut = LexerUnitTest()
 
-    with open('tests\lexer\LexerTests.yaml', 'r') as file:
+    with open('tests\lexer\LexerTests.yml', 'r') as file:
         data = yaml.safe_load(file)
-        print(data)
-        #print(ut.run_tests(lines_to_test[0]))
+        last_test = next(reversed(data))
+        last_test_num = int("".join(filter(str.isdigit, last_test)))
+
+    for test in range(last_test_num):
+        expected_tokens = []
+        output_tokens = ut.run_test(data[f'test{test+1}'])
+        for token in data[f'expected{test+1}']:
+            expected_tokens.append(token)
+
+        assert output_tokens == expected_tokens
+        print(f"Test Case {test+1}: PASS")
