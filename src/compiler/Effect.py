37baba5d6618b0eabafe4be122dbf90e3ce6c7db
Ian Cooper <iancoop25@gmail.com>
2026-04-08 15:42:49 -0500

    Remove old comment

--- src/compiler/Effect.py
+++ src/compiler/Effect.py
@@ -2,6 +2,5 @@ from enum import Enum
 
 class Effect:
     def __init__(self, effect_type: str, param: list[str]):
-        # todo switch to enum
         self.type = effect_type
         self.param = param
