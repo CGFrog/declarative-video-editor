d7ba52a2c83ea7a169a9c0e57b7be4eb717505b7
CameronAron <cameron.aron04@gmail.com>
2026-04-15 14:48:42 -0500

    first successful exe build with good vlc importing that doesn't need to have the vlc app downloaded

--- src/ui/VideoPlayer.py
+++ src/ui/VideoPlayer.py
@@ -1,12 +1,20 @@
 import tkinter as tk
 import vlc
 import os
+import sys
 from tkinter import filedialog
 from tkinter import ttk
 
 class VideoPlayer:
     def __init__(self, parent_frame):
-        self.instance = vlc.Instance()
+        vlc_path = self.get_vlc_path()
+
+        os.environ["PATH"] = vlc_path + ";" + os.environ.get("PATH", "")
+
+        self.instance = vlc.Instance([
+            f"--plugin-path={os.path.join(vlc_path, 'plugins')}"
+        ])
+
         self.player = self.instance.media_player_new()
 
         self.parent_frame = parent_frame
@@ -31,6 +39,14 @@ class VideoPlayer:
         self.build_ui()
         self.parent_frame.after(100, self.setVideoOutput)
         self.parent_frame.after(200, self.update_progress)
+
+    def get_vlc_path(self):
+        if getattr(sys, 'frozen', False):
+            base_path = sys._MEIPASS
+        else:
+            base_path = os.path.dirname(__file__)
+
+        return os.path.join(base_path, "vlc")
 
     def build_ui(self):
         self.video_widget = tk.Frame(self.parent_frame, bg="black")
