5111a401e87749c9f6018eec2fdec905ef05e62a
ben-burie <benburie22@gmail.com>
2026-04-26 10:31:54 -0500

    Delete unused resolved text element

--- src/compiler/ResolvedTextElement.py
+++
@@ -1,6 +0,0 @@
-class ResolvedTextElement():
-    def __init__(self, text, duration, timeline_start, z):
-        self.text = text
-        self.duration = duration
-        self.timeline_start = timeline_start
-        self.z = z
