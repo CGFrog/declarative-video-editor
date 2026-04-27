2febdc762e239c11dad64781f663f17b712a834b
kylekocourek <kkocourek43@gmail.com>
2026-04-26 20:21:19 -0500

    Change to stateVariable

--- src/compiler/StateVariable.py
+++ src/compiler/StateVariable.py
@@ -11,8 +11,8 @@ class Clip:
         return False
 
 class StateVariable:
-    def __init__(self, effects=None, clips=None, type=""):
-        self.effects: list[Effect] = effects if effects is not None else []
+    def __init__(self, effects=[], clips=[], type=""):
+        self.effects: list[Effect] = effects
         self.clips: list[Clip] = clips if clips is not None else []
         self.type: str = type
 
