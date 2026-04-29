ec8ca26bffb0baab642ec2f22d4454a84cd9e4b7
CameronAron <cameron.aron04@gmail.com>
2026-04-29 14:37:13 -0500

    Render no longer blocks the main thread and the video auto plays after rendering

--- src/ui/Menu.py
+++ src/ui/Menu.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import subprocess
+import threading
 from src.compiler.Compiler import Compiler
 
 class Menu(tk.Menu):
@@ -31,13 +32,20 @@ class Menu(tk.Menu):
         command = Compiler().compile(content)
 
     def render(self):
+        threading.Thread(target=self.render_task, daemon=True).start()
+
+    def render_task(self):
         compiler = Compiler()
         content = self.text_editor.get_content()
         command = compiler.compile(content)
         output_path = compiler.parser.render_settings.export_path
         subprocess.run(command, shell=True, check=True)
         print("--- Created: " + output_path + " ---")
-        self.video_player.load(output_path)
+        self.after(0, lambda: self.load_and_play(output_path))
+
+    def load_and_play(self, path):
+        self.video_player.load(path)
+        self.after(100, self.video_player.play)
 
 
     def save(self):
