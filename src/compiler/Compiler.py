cf2896e1db8007a861a73ad3f1f71cceb9820da6
Ian Cooper <iancoop25@gmail.com>
2026-04-29 23:01:02 -0500

    Removed compiler main, use UI instead for testing

--- src/compiler/Compiler.py
+++ src/compiler/Compiler.py
@@ -255,39 +255,3 @@ class Compiler:
         ffmpeg_builder.final_video_label = current_v # current_v holds label of text overlay
 
         return filter_parts_extra
-
-def main():
-    source_code = """
-    video karl = "C:\\Users\\benbu\\Videos\\DVEL_TEST\\karl.mkv" (0,4)
-    video scenery1 = "C:\\Users\\benbu\\Videos\\DVEL_TEST\\ben2.MOV" (0, e)
-    video scenery2 = "C:\\Users\\benbu\\Videos\\DVEL_TEST\\ben1.MOV" (0, e)
-    video zach = "C:\\Users\\benbu\\Videos\\DVEL_TEST\\zach.mkv" (3, 6) |> volume(0)
-    audio strike = "C:\\Users\\benbu\\Videos\\DVEL_TEST\\strike_sound_effect.mp3" (0, 4) |> volume(0)
-    str karl_caption = "Here is Karl!" 3
-    str zach_caption = "Here is Zach!" 3
-
-    timeline
-
-    ian 0 1
-    ian_caption 1 1
-    scenery1 after ian 1
-    zach after scenery1 2
-    zach_caption 6 2
-    scenery2 after zach 2
-    strike 3 3
-    
-    render "output.mp4" [1920,1080]
-    """
-
-    compiler = Compiler()
-    command = compiler.compile(source_code)
-    print(command)
-    subprocess.run(
-        args=command, 
-        shell=True, 
-        check=True
-    )
-
-
-if __name__ == "__main__":
-    main()
