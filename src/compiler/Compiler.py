1b1a4788cd19f52efebc85b6f078436e9b328960
Ian Cooper <iancoop25@gmail.com>
2026-05-06 22:09:44 -0500

    remove old comment

--- src/compiler/Compiler.py
+++ src/compiler/Compiler.py
@@ -131,7 +131,6 @@ class Compiler:
             if (end < start):
                 raise Exception(f"Clip {clip.path} ({start},{end}) cannot have negative duration.")
             
-            # I think this will break, probably meant state.effects?
             effects = state.effects
 
             for e in effects:
